9a7172080d4ee509d9b8bb38ed05db544d75ebfb
Zy <3935823763@qq.com>
2026-08-06 14:28:43 +0800

    Update site content from admin

--- site-content.js
+++ site-content.js
@@ -108,6 +108,17 @@ window.SITE_CONTENT = {
       "primary": true,
       "download": "",
       "url": "https://hk.gh-proxy.com/https://github.com/Zy-api/AppShare/releases/download/Az888/HPQl.zip"
+    },
+    {
+      "icon": "cloud",
+      "title": "新文件",
+      "description": "这里填写文件说明。",
+      "type": "FILE",
+      "meta": "可下载",
+      "button": "立即下载",
+      "primary": true,
+      "download": "",
+      "url": "#"
     }
   ]
 };
