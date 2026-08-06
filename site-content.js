bcfc28e2f8e5662ad1ca1bb43c882252b4924e01
Zy <3935823763@qq.com>
2026-08-06 21:00:26 +0800

    Update site content from admin

--- site-content.js
+++ site-content.js
@@ -118,7 +118,7 @@ window.SITE_CONTENT = {
     {
       "icon": "folder",
       "title": "VT内核[和平精英]",
-      "description": "VT内核，拥有硬件断点追踪，透视自瞄掩体漏打等功能",
+      "description": "VT内核-拥有硬件断点追踪，透视自瞄掩体漏打等功能",
       "type": "ZIP",
       "meta": "可下载",
       "button": "立即下载",
